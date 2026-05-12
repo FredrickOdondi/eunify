9e01af231ddb1350294060ce532e4916b5ad0bda
FredrickOdondi <fredrickodondi@users.noreply.github.com>
2026-05-13 00:33:36 +0300

    Initial Commit

---
+++ eunify/eunifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eunifyApp.swift
+//  eunify
+//
+//  Created by fredrickodondi on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct eunifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
